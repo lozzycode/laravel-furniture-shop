5d6a321fe7d1cb273d343b3883ba04f584b19986
Laurence <lozrecs@hotmail.co.uk>
2024-05-31 16:23:54 +0100

    database/migrations/2024_05_31_141013_create_products_table.php & routes/web.php update

--- database/migrations/2024_05_31_141013_create_products_table.php
+++ database/migrations/2024_05_31_141013_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id');
             $table->integer('width');
             $table->integer('height');
             $table->integer('depth');
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use \App\Http\Controllers\ProductController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProductController::class, 'allProducts']);
