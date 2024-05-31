8dea555ef04f7f69adf51411a23e628369583748
Laurence <lozrecs@hotmail.co.uk>
2024-05-31 16:49:05 +0100

    app/Http/Controllers/ProductController.php updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function allProducts(Request $request)
+
+    {
+        if (isset($request->instock)) {
+            $stock = $request->instock;
+            $products = Product::where('stock', '=', $stock)->get();
+        }  else {
+            $products = Product::all();
+        }
+        return view('products', ['products' => $products]);
+    }
 }
